aa82a2abddfdf4c8a2e992e4d7152c522ed74357
Mohammad Ariawan (Ari) <malatasf@gmail.com>
2018-02-05 16:13:38 +0700

    Add PermitItem Purchase Data

---
+++ src/main/java/com/ptff/qsystem/data/PermitItem.java
@@ -0,0 +1,33 @@
+package com.ptff.qsystem.data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="item_permit")
+public class PermitItem {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	
+	@Column(name="name")
+	@NotNull
+	@NotEmpty
+	private String name;
+	
+	@Column(name="description")
+	@NotNull
+	@NotEmpty
+	private String description;
+}
