9ef36853ef2d7fa10c1ddf1a6751372b24c4de82
mohammad.ariawan <mohammad.ariawan@kangaroo-service.com>
2018-03-21 17:27:08 +1100

    Notification HTML Template

--- src/main/java/com/ptff/qsystem/web/NotificationController.java
+++ src/main/java/com/ptff/qsystem/web/NotificationController.java
@@ -1,5 +1,24 @@
 package com.ptff.qsystem.web;
 
-public class NotificationController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+public class NotificationController {
+	@RequestMapping(value="/notifications", params="group")
+	public String groupNotification(@RequestParam("group") String group, Model model) {
+			    
+		return "/notification/notifications :: notifications";
+	}
+	
+	@RequestMapping(value="/notifications", params="user")
+	public String userNotification(@RequestParam("user") String user, Model model) {
+			    
+		return "/notification/notifications :: notifications";
+	}
+	
+	
 }
